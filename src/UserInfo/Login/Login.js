87dbe0faf8cc58f74a8eb5593a299a10f803a01f
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-19 20:29:19 +0600

    firebase and env finished

--- src/UserInfo/Login/Login.js
+++ src/UserInfo/Login/Login.js
@@ -4,13 +4,15 @@ import { useForm } from "react-hook-form";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const handleLogin = (data) => {
+    console.log(data);
+  }
 
   return (
     <div className='h-[700px] flex justify-center items-center'>
       <div className='outline rounded-lg p-5 w-80'>
-        <p className='text-4xl text-center p-3 font-bold'>Login</p>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <p className='text-4xl text-center p-2 font-bold'>Login</p>
+        <form onSubmit={handleSubmit(handleLogin)}>
           {/* <Header /> */}
           <div className="form-control w-full">
             <label className="label">
@@ -24,10 +26,9 @@ const Login = () => {
             </label>
             <input type='password' {...register("email")} placeholder="Email" className="input input-bordered w-full" />
           </div>
-          {/* <p>{data}</p> */}
           {/* <input type="submit" /> */}
           <div className='flex justify-center'>
-            <button className="btn btn-active btn-accent mt-3 w-full">Log in</button>
+            <button className="btn btn-active btn-accent mt-3 w-full text-white">Log in</button>
           </div>
           <div className="divider">OR</div>
           <div className='flex justify-center'>
