fc46b991c222d83826ef5eedaf1f9582efd4730b
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-20 00:38:20 +0600

    firebase conncted with signup page

--- src/UserInfo/Login/Login.js
+++ src/UserInfo/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 // import Header from "./Header";
 
 const Login = () => {
@@ -10,7 +11,7 @@ const Login = () => {
 
   return (
     <div className='h-[700px] flex justify-center items-center'>
-      <div className='outline rounded-lg p-5 w-80'>
+      <div className='outline rounded-lg p-5 w-96'>
         <p className='text-4xl text-center p-2 font-bold'>Login</p>
         <form onSubmit={handleSubmit(handleLogin)}>
           {/* <Header /> */}
@@ -32,6 +33,7 @@ const Login = () => {
           <div className='flex justify-center'>
             <button className="btn btn-active btn-accent mt-3 w-full text-white">Log in</button>
           </div>
+          <p className='p-1 text-center'>New in Doctor's? Go <Link to='/signup' className='text-cyan-600 font-bold'>Sign up</Link></p>
           <div className="divider">OR</div>
           <div className='flex justify-center'>
             <button className="btn btn-primary w-full uppercase">log in with Google</button>
--- src/UserInfo/SignUp/SignUp.js
+++ src/UserInfo/SignUp/SignUp.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const SignUp = () => {
+  const { createUser } = useContext(AuthContext);
+
   const { register, formState: { errors }, handleSubmit } = useForm();
   const handleLogin = (data) => {
     console.log(data);
+    createUser(data.email, data.name)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(err => console.error(err))
   }
   return (
     <div className='h-[700px] flex justify-center items-center'>
-      <div className='outline rounded-lg p-5 w-80'>
+      <div className='outline rounded-lg p-5 w-96'>
         <p className='text-4xl text-center p-2 font-bold'>Sign Up</p>
         <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
-            <input type='text' {...register("name", { required: "Name is required", minLength: { value: 12, message: "Your name is too short" } })} placeholder="Name" className="input input-bordered w-full" />
+            <input type='text' {...register("name", {
+              required: "Name is required",
+              minLength: { value: 12, message: "Your name is too short" }
+            })} placeholder="Name" className="input input-bordered w-full" />
             {errors.name && <p role="alert" className='text-red-600'>{errors.name?.message}</p>}
           </div>
           <div className="form-control w-full">
@@ -31,7 +44,6 @@ const SignUp = () => {
             </label>
             <input type='password' {...register("password", {
               required: "Password is required",
-              minLength: { value: 6, message: "Password should 6 character length" },
               pattern: { value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, message: "Minimum eight characters, at least one letter and one number" }
             })} placeholder="Password" className="input input-bordered w-full" />
             {errors.password && <p role="alert" className='text-red-600'>{errors.password?.message}</p>}
@@ -39,6 +51,7 @@ const SignUp = () => {
           <div className='flex justify-center'>
             <button className="btn btn-active btn-accent mt-3 w-full text-white">Sign Up</button>
           </div>
+          <p className='p-1 text-center'>Already have an account? Go <Link to='/login' className='text-cyan-600 font-bold'>Sign in</Link></p>
           <div className="divider">OR</div>
           <div className='flex justify-center'>
             <button className="btn btn-primary w-full uppercase">log in with Google</button>
