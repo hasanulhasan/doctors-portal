cd6c181c2a25df692e19ada9304b725cb23059cb
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-22 20:36:24 +0600

    booking showing on table

--- src/Pages/DashBoard/MyAppointment/MyAppointment.js
+++ src/Pages/DashBoard/MyAppointment/MyAppointment.js
@@ -1,39 +1,44 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 
 const MyAppointment = () => {
+  const { user } = useContext(AuthContext);
+  const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
+  const { data: bookings = [] } = useQuery({
+    queryKey: ['bookings', user?.email],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data;
+    }
+
+  })
+
+
   return (
     <div>
-      <h1 className='text-3xl text center'>My Appointment</h1>
+      <h1 className='text-3xl text center mb-3'>My Appointment</h1>
       <div className="overflow-x-auto">
         <table className="table w-full">
           <thead>
             <tr>
               <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Treatment</th>
+              <th>Date</th>
+              <th>Time</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-
-            <tr className="hover">
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
-            </tr>
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
+            {
+              bookings.map((book, i) => <tr>
+                <th>{i}</th>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>Purple</td>
+              </tr>)
+            }
           </tbody>
         </table>
       </div>
